bb3fe62f1a7a77873166694b6c21b80131d371d8
池田直人 <ike.climb7010@gmail.com>
2020-01-19 14:19:27 +0900

    no message

---
+++ resources/js/components/parentStepDetail/parentStepDetailMainColumn.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="l-row__col12 l-row__col08-pc u-pr--l u-pl--l">
+        <!-- 親STEP詳細 -->
+        <parentStepDetailItem
+            :user="user"
+            :parentStep="parentStep"
+            :challengeFlg="challengeFlg"
+        ></parentStepDetailItem>
+        <!-- チャレンジボタン -->
+        <challengeBtn
+            :parentStepId="parentStep.id"
+            :childStepId="parentStep.child_steps[0]['id']"
+            :user="user"
+            :challengeFlg="challengeFlg"
+        ></challengeBtn>
+        <!-- 子STEPインデックス -->
+        <childStepIndex
+            :childSteps="parentStep.child_steps"
+        ></childStepIndex>
+        <!-- チャレンジボタン -->
+        <!-- <challengeBtn
+            :parentStepId="parentStep.id"
+            :childStepId="parentStep.child_steps[0]['id']"
+            :user="user"
+            :challengeFlg="challengeFlg"
+        ></challengeBtn> -->
+    </div>
+</template>
+
+<script>
+    import parentStepDetailItem from './parentStepDetailItem';
+    import childStepIndex from '../childStepIndex/childStepIndex';
+    import challengeBtn from './challengeBtn';
+    export default {
+        components: {
+            parentStepDetailItem,
+            childStepIndex,
+            challengeBtn,
+        },
+        props: ['parentStep', 'user', 'challengeFlg'],
+    }
+</script>
