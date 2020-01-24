c811d32a6c81145f15c18e8cabceea5536a82965
池田直人 <ike.climb7010@gmail.com>
2020-01-25 00:12:30 +0900

    no message

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,18 +14,18 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['name' => '英語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => '中国語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'フランス語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'ドイツ語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'その他外国語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => '言語', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => '数学', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => '化学', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => '経済', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => '歴史', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => 'プログラミング', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'デザイン', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'ファッション', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => '政治', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => '投資', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => 'ビジネス', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => '心理学', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'デザイン', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'その他', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ];
         DB::table('categories')->insert($data);
     }
