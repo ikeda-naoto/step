0c2c6851138af6125342ef786a27018295f2c4c6
池田直人 <ike.climb7010@gmail.com>
2020-02-02 21:13:50 +0900

    404ページ編集

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,17 +6,14 @@
 <div class="l-container u-bg--light">
     <div class="l-site-width">
         <div class="p-error404">
-            <div class="p-error404__inner">
-                <h1 class="c-title--normal p-error404__title">404 Page Not Found</h1>
-                <p class="p-error404__text">
-                    STEPをご利用いただきありがとうございます。<br>
-                    申し訳ございませんが、お探しのページは存在しないか、削除された可能性があります。
-                </p>
-                <div class="p-error404__img">
+            <h1 class="c-title--normal">404 Page Not Found</h1>
+            <p class="p-error404__text">
+                STEPをご利用いただきありがとうございます。<br>
+                申し訳ございませんが、お探しのページは存在しないか、削除された可能性があります。
+            </p>
+            <div class="p-error404__img">
                 <img src="{{ asset('images/logo_icn.png') }}" alt="">
-                </div>
             </div>
-
         </div>
     </div>
 </div>
